a0a5b549f0d5862283d96d39140816225ed525ef
max_rad <maxim_radionov@bk.ru>
2022-03-22 14:00:16 +0300

    Исправляет поведение меню

--- source/js/modules/menu/menu.js
+++ source/js/modules/menu/menu.js
@@ -1,10 +1,12 @@
 const showMenu = () => {
   const navMain = document.querySelector('[data-menu="menu"]');
   const navToggle = navMain.querySelector('[data-menu="toggle"]');
+  const anchorLinks = navMain.querySelectorAll('[data-link]');
 
   navMain.classList.remove('navigation--no-js');
   navToggle.addEventListener('click', function (evt) {
     evt.preventDefault();
+
     if (navMain.classList.contains('navigation--is-closed')) {
       navMain.classList.remove('navigation--is-closed');
       navMain.classList.add('navigation--is-opened');
@@ -13,6 +15,21 @@ const showMenu = () => {
       navMain.classList.add('navigation--is-closed');
     }
   });
+
+  window.addEventListener('mouseup', (evt) => {
+    if (!navMain.contains(evt.target)) {
+      navMain.classList.remove('navigation--is-opened');
+      navMain.classList.add('navigation--is-closed');
+    }
+  });
+
+  for (let anchor of anchorLinks) {
+    anchor.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      navMain.classList.remove('navigation--is-opened');
+      navMain.classList.add('navigation--is-closed');
+    });
+  }
 };
 
 export {showMenu};
